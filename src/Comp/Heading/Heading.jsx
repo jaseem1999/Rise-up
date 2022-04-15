cd7cf1b673a169f209502d55cf0482740d615358
Adil <ajkkalappatt@gmail.com>
2022-04-16 00:32:35 +0530

    added all without materials and about

--- src/Comp/Heading/Heading.jsx
+++ src/Comp/Heading/Heading.jsx
@@ -5,6 +5,23 @@ function Heading() {
   return (
     <footer className="heading">
         <div className="heading__title"><span className="rise">Rise</span><br/><span className="up">Up</span></div>
+        <div className="small-heading-left">
+          <p>
+            Rise Up<br/>
+            since 2021,<br/>
+            <a href="https://chat.whatsapp.com/GaxkSjhC0n69qBf6yefCFZ">Rise Up WhatsApp Group</a>,<br/>
+            Rise is part of the comrades Muhamme Mustafa Study Circle.<br/>
+      
+          </p>
+        </div>
+        <div className="small-heading">
+          <p>
+            Rise Up<br/>
+            Providing study material,<br/>
+            Exam info,<br/>
+            Student support,<br/>
+          </p>
+        </div>
     </footer>
   )
 }
